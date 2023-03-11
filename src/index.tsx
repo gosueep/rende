1e795626bc88af2d593ecfd398c99b1016e86e35
gosueep <eugin03@gmail.com>
2023-03-11 10:15:42 -0700

    lazy loading

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
 import type { Component } from 'solid-js';
+import { lazy } from "solid-js";
 import { Router, Routes, Route } from '@solidjs/router'
 import { render } from 'solid-js/web';
 
-import SearchPage from './SearchPage';
-import EventPage from './EventPage';
+// import SearchPage from './SearchPage';
+// import EventPage from './EventPage';
+
+const SearchPage = lazy(() => import("./SearchPage"))
+const EventPage = lazy(() => import ("./EventPage"))
 
 const App: Component<{}> = () => {
 	 return (
 		<Router>
 			<Routes>
       			<Route path='/' component={ SearchPage } />
-				<Route path='/event1' component={ EventPage } />
+				<Route path="/event/:id" component={ EventPage } />
 			</Routes>
     	</Router>
 	 )
