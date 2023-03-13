20dd065e8afdf5d7fece0fd8e135051d26678687
gosueep <eugin03@gmail.com>
2023-03-12 18:44:13 -0600

    small index changes + bcrypt in package

--- src/index.tsx
+++ src/index.tsx
@@ -5,19 +5,24 @@ import { render } from 'solid-js/web';
 
 const SearchPage = lazy(() => import("./SearchPage"))
 const EventPage = lazy(() => import ("./EventPage"))
+const DashboardPage  = lazy(() => import("./DashboardPage"))
+const LoginPage = lazy(() => import("./LoginPage"))
+
+// CONVERTED TO LAZY LOADING, pages only rendered when clicked as opposed to upon startup
+// 	Might not matter at all, so feel free to revert.
 // import SearchPage from './SearchPage'
 // import EventPage from './EventPage'
-import DashboardPage from './DashboardPage'
-import LoginPage from './LoginPage'
+// import DashboardPage from './DashboardPage'
+// import LoginPage from './LoginPage'
 
 const App: Component<{}> = () => {
 	 return (
 		<Router>
 			<Routes>
-				{/* <Route path="/event/:id" component={ EventPage } /> */}
-				<Route path='/' component={ LoginPage } />
-      			<Route path='/SearchPage' component={ SearchPage } />
-				<Route path='/' component={ DashboardPage } />
+      			<Route path='/' component={ SearchPage } />
+				<Route path='/event/:id' component={ EventPage } />
+				<Route path='/login' component={ LoginPage } />
+				<Route path='/dashboard' component={ DashboardPage } />
 			</Routes>
     	</Router>
 	 )
