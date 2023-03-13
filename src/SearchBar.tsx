6b48a3d67843f6cebd5a6ceeeb2754a56cf4ab1c
gosueep <eugin03@gmail.com>
2023-03-12 21:07:57 -0600

    finish navbar for mvp and search page

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -6,10 +6,10 @@ export default function SearchBar() {
 				<div class="flex-1" id="searchbar">
 					<div class="flex flex-row">
 						<div class="flex-1">
-							<input type="search" class="p-2.5 w-full align-top text-sm text-gray-900 bg-gray-300 border border-gray-300" placeholder="Search for events" required={true} />
+							<input type="search" class="p-2.5 w-full align-top text-sm text-gray-900 bg-gray-300 border border-gray-300 rounded" placeholder="Search for events" required={true} />
 						</div>
 						<div class="flex-none"></div>
-						<button type="submit" class="p-2.5 align-top text-sm font-medium text-white bg-blue-700 border border-blue-700">
+						<button type="submit" class="p-2.5 align-top text-sm font-medium text-white bg-blue-700 border border-blue-700 rounded">
 							<svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
 							</svg>
@@ -17,7 +17,8 @@ export default function SearchBar() {
 						</button>
 					</div>
 				</div>
-				<div class="flex-none" id="categories">
+				{/* Add back filtering when we have time. For MVP, maybe not necessary... */}
+				{/* <div class="flex-none" id="categories">
 					<button type="submit" class="p-2 align-top text-md font-medium text-white bg-yellow-600 border border-yellow-600">
 						<span>Categories</span>
 					</button>
@@ -31,7 +32,7 @@ export default function SearchBar() {
 					<button type="submit" class="p-2 align-top text-md font-medium text-white bg-teal-800 border border-teal-800">
 						<span>Sort</span>
 					</button>
-				</div>
+				</div> */}
 			</div>
 		</div>
 	);
