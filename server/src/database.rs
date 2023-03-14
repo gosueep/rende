7c52598d6ec95128f848b6e221e15b9693347c88
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2023-03-14 03:20:57 -0500

    Start adding database features

--- server/src/database.rs
+++ server/src/database.rs
@@ -1,18 +1,18 @@
-use diesel::pg::PgConnection;
+use chrono::*;
+use diesel::pg::{Pg, PgConnection};
 use diesel::prelude::*;
+use serde::de::Error;
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 use std::env;
 
+// Overarching club (some events are a part of clubs, some aren't)
 diesel::table! {
-    clubs (id) {
-        id -> Int4,
+    club (id) {
+        id -> Int8,
         name -> Varchar,
-        description -> Text,
-    }
-
-	events (id) {
-        id -> Int4,
-        name -> Varchar,
-        description -> Text,
+        description_text -> Varchar,
+        description_html -> Varchar,
     }
 }
 
@@ -20,18 +20,180 @@ diesel::table! {
 pub struct Club {
     pub id: i64,
     pub name: String,
-    pub description: String,
+    pub description_text: String,
+    pub description_html: String,
+}
+
+// Variable number of club images
+diesel::table! {
+    club_image (id) {
+        id -> Int8,
+        club_id -> Int8,
+        index -> Int8,
+        png -> Blob,
+    }
+}
+
+#[derive(Queryable)]
+pub struct ClubImage {
+    pub id: i64,
+    pub club_id: i64,
+    pub index: i64,
+    pub png: Vec<u8>,
+}
+
+// Variable number of club organizers, some may have more permissions (TODO)
+diesel::table! {
+    club_organizer (id) {
+        id -> Int8,
+        club_id -> Int8,
+        user_id -> Int8,
+    }
+}
+
+#[derive(Queryable)]
+pub struct ClubOrganizer {
+    pub id: i64,
+    pub club_id: i64,
+    pub user_id: i64,
+}
+
+// A specific event, which may be under a club
+diesel::table! {
+    event (id) {
+        id -> Int8,
+        club_id -> Nullable<Int8>,
+        name -> Varchar,
+        description_text -> Varchar,
+        description_html -> Varchar,
+        start -> Timestamp,
+    }
 }
 
 #[derive(Queryable)]
 pub struct Event {
     pub id: i64,
+    pub club_id: Option<i64>,
     pub name: String,
-    pub description: String,
+    pub description_text: String,
+    pub description_html: String,
+    pub start: NaiveDateTime,
 }
 
-async fn start_database() -> PgConnection {
-	let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+// An event category, every event can have as many categories as neccesary
+diesel::table! {
+    event_category (id) {
+        id -> Int8,
+        event_id -> Int8,
+        category -> Varchar,
+    }
+}
+
+#[derive(Queryable)]
+pub struct EventCategory {
+    pub id: i64,
+    pub event_id: i64,
+    pub category: String,
+}
+
+// An event rsvp, different tiers of rsvps
+diesel::table! {
+    event_rsvp (id) {
+        id -> Int8,
+        event_id -> Int8,
+        user_id -> Int8,
+    }
+}
+
+#[derive(Queryable)]
+pub struct EventRsvp {
+    pub id: i64,
+    pub event_id: i64,
+    pub user_id: i64,
+}
+
+// Variable number of event organizers, some may have more permissions (TODO)
+diesel::table! {
+    event_organizer (id) {
+        id -> Int8,
+        event_id -> Int8,
+        user_id -> Int8,
+    }
+}
+
+#[derive(Queryable)]
+pub struct EventOrganizer {
+    pub id: i64,
+    pub event_id: i64,
+    pub user_id: i64,
+}
+
+// Users, some of which are organizers
+diesel::table! {
+    user (id) {
+        id -> Int8,
+        name -> Varchar,
+    }
+}
+
+#[derive(Queryable)]
+pub struct User {
+    pub id: i64,
+    pub name: String,
+}
+
+// Serializable structs for JSON transer
+#[derive(Queryable, Serialize, Deserialize)]
+pub struct EventJson {
+    pub name: String,
+    pub description: String,
+    pub start: i64,
+}
+
+#[derive(Queryable, Serialize, Deserialize)]
+pub struct EventResultJson {
+    pub events: Vec<EventJson>,
+}
+
+async fn create_database() -> PgConnection {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+async fn get_all_events(conn: &mut PgConnection) -> Option<Vec<Event>> {
+    let result = event::table.limit(10).get_results::<Event>(conn);
+    if let Ok(events) = result {
+        Some(events)
+    } else {
+        None
+    }
+}
+
+async fn get_all_events_json(conn: &mut PgConnection) -> Option<String> {
+    // Query 10 events for now
+    let result: Result<Vec<Event>, diesel::result::Error> =
+        event::table.limit(10).get_results(conn);
+    if result.is_err() {
+        return None;
+    }
+    let events = result.unwrap();
+
+    let events_result = EventResultJson {
+        events: events
+            .iter()
+            .map(|event| EventJson {
+                name: event.name.clone(),
+                description: event.description_text.clone(),
+                start: event.start.timestamp(),
+            })
+            .collect(),
+    };
+
+    let json = serde_json::to_string(&events_result);
+    if json.is_err() {
+        return None;
+    }
+
+    Some(json.unwrap())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,10 @@
 mod database;
 
-use database::{start_database};
-
-use actix_files as fs;
 use actix_cors::Cors;
-use actix_web::{middleware, App, HttpServer, HttpResponse, Responder, get, post, web, http};
-use serde::{Serialize, Deserialize};
+use actix_files as fs;
+use actix_web::{get, http, middleware, post, web, App, HttpResponse, HttpServer, Responder};
+use dotenvy::dotenv;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Login {
@@ -29,24 +28,42 @@ struct Club {
 
 #[post("/login")]
 async fn login(info: web::Json<Login>, db_conn: web::Data<PgConnection>) -> impl Responder {
-	println!("Received login request: {:?}", info);
+    println!("Received login request: {:?}", info);
     let login = info.into_inner();
-    
-	//Check email and password against database, return user_id if successful
 
-    let response = LoginResponse { success: true, user_id: 1 };
+    //Check email and password against database, return user_id if successful
+
+    let response = LoginResponse {
+        success: true,
+        user_id: 1,
+    };
     HttpResponse::Ok().json(response)
 }
 
 #[get("/get_clubs")]
 async fn get_clubs() -> impl Responder {
     let clubs = vec![
-        Club { id: "1".to_string(), name: "Club 1".to_string(), meeting_time: "Monday 6pm".to_string(), description: "A club for testing purposes".to_string() },
-        Club { id: "2".to_string(), name: "Club 2".to_string(), meeting_time: "Tuesday 7pm".to_string(), description: "Another club for testing purposes".to_string() },
-        Club { id: "3".to_string(), name: "Club 3".to_string(), meeting_time: "Wednesday 8pm".to_string(), description: "A third club for testing purposes".to_string() },
+        Club {
+            id: "1".to_string(),
+            name: "Club 1".to_string(),
+            meeting_time: "Monday 6pm".to_string(),
+            description: "A club for testing purposes".to_string(),
+        },
+        Club {
+            id: "2".to_string(),
+            name: "Club 2".to_string(),
+            meeting_time: "Tuesday 7pm".to_string(),
+            description: "Another club for testing purposes".to_string(),
+        },
+        Club {
+            id: "3".to_string(),
+            name: "Club 3".to_string(),
+            meeting_time: "Wednesday 8pm".to_string(),
+            description: "A third club for testing purposes".to_string(),
+        },
     ];
 
-	//Return id, name, meeting_time, description, image url
+    //Return id, name, meeting_time, description, image url
 
     HttpResponse::Ok()
         .content_type("application/json")
@@ -54,70 +71,70 @@ async fn get_clubs() -> impl Responder {
 }
 
 #[get("/set_club_images")]
-async fn get_clubs() -> impl Responder {
-	//Allow admin to set image url for each club
+async fn get_club_images() -> impl Responder {
+    //Allow admin to set image url for each club
 }
 
 #[get("/get_events")]
 async fn get_events() -> impl Responder {
-	HttpResponse::Ok()
-	.content_type("application/json")
-	.body("[{\"name\":\"test\"}]")
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body("[{\"name\":\"test\"}]")
 }
 
 #[get("/get_event/{event_id}")]
 async fn get_eventById(path: web::Path<(String)>) -> impl Responder {
-	let event_id = path.into_inner();
+    let event_id = path.into_inner();
 
-	let eventinfo = vec![
-		{id: "test-UFJKDJFSDF",
-		name: "Test Event",
-		datetime: new Date(Date.now()),
-		location: "Marquez 123",
-		description: "We meeting to plan stuff uhhh :)",
-		attendees: ["Eugin", "NotEugin"],
-		is_recurring: false}
-	];
+    let event_info = vec![
+        {id: "test-UFJKDJFSDF",
+        name: "Test Event",
+        datetime: new Date(Date.now()),
+        location: "Marquez 123",
+        description: "We meeting to plan stuff uhhh :)",
+        attendees: ["Eugin", "NotEugin"],
+        is_recurring: false}
+    ];
 
-	HttpResponse::Ok()
-		.content_type("application/json")
-		.body(serde_json::to_string(&eventinfo).unwrap())
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(serde_json::to_string(&event_info).unwrap())
 }
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("DATABASE_URL", "postgres://postgres:x6hjwU3tJ2jrC6P@database-1.c8o82ejnfjrp.us-east-2.rds.amazonaws.com:5432/postgres");
-    let db_conn = start_database().await;
-	HttpServer::new(|| {
-		App::new()
-			.wrap(middleware::Compress::default())
-			.wrap(middleware::Logger::default())
-			.wrap(
+    dotenv().ok();
+    let db_conn = create_database().await;
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Compress::default())
+            .wrap(middleware::Logger::default())
+            .wrap(
                 Cors::permissive()
                     .allowed_origin("http://localhost:9000")
                     .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
                     .allowed_headers(vec![
-						http::header::AUTHORIZATION,
-						http::header::ACCEPT,
-						http::header::CONTENT_TYPE,
-					])
-					.max_age(3600)
+                        http::header::AUTHORIZATION,
+                        http::header::ACCEPT,
+                        http::header::CONTENT_TYPE,
+                    ])
+                    .max_age(3600),
             )
-			.service(get_events)
-			.service(get_clubs)
-			.app_data(web::Data::new(db_conn.clone()))
-			.service(login)
-			//.service(web::resource("/").to(|req: HttpRequest| async move {
-			//	fs::NamedFile::open_async("../public/index.html").await.unwrap().into_response(&req)
-			//}))
-			.service(
-				fs::Files::new("/", "../public")
-					.index_file("index.html")
-					.show_files_listing(),
-			)
-	})
-	.bind(("127.0.0.1", 3030))?
-	.run()
-	.await
+            .service(get_events)
+            .service(get_clubs)
+            .service(get_club_images)
+            .app_data(web::Data::new(db_conn.clone()))
+            .service(login)
+            //.service(web::resource("/").to(|req: HttpRequest| async move {
+            //	fs::NamedFile::open_async("../public/index.html").await.unwrap().into_response(&req)
+            //}))
+            .service(
+                fs::Files::new("/", "../public")
+                    .index_file("index.html")
+                    .show_files_listing(),
+            )
+    })
+    .bind(("127.0.0.1", 3030))?
+    .run()
+    .await
 }
