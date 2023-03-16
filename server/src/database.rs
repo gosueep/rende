d751081bd9036c2615220d21cde0691a86609916
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2023-03-15 23:06:33 -0600

    Fix some stuff

--- server/src/database.rs
+++ server/src/database.rs
@@ -9,326 +9,400 @@ use std::{env, ptr};
 
 // Overarching club (some events are a part of clubs, some aren't)
 diesel::table! {
-    club (id) {
-        id -> Int8,
-        name -> Varchar,
-        description_text -> Varchar,
-        description_html -> Varchar,
-    }
+	club (id) {
+		id -> Int8,
+		name -> Varchar,
+		description_text -> Varchar,
+		description_html -> Varchar,
+	}
 }
 
 #[derive(Queryable)]
 pub struct Club {
-    pub id: i64,
-    pub name: String,
-    pub description_text: String,
-    pub description_html: String,
+	pub id: i64,
+	pub name: String,
+	pub description_text: String,
+	pub description_html: String,
 }
 
 #[derive(Queryable, Serialize, Deserialize)]
 pub struct ClubJson {
-    pub id: i64,
-    pub name: String,
-    pub description_text: String,
-    pub description_html: String,
+	pub id: i64,
+	pub name: String,
+	pub description_text: String,
+	pub description_html: String,
 }
 
 // Variable number of club images
 diesel::table! {
-    club_image (id) {
-        id -> Int8,
-        club_id -> Int8,
-        index -> Int8,
-        png -> Blob,
-    }
+	club_image (id) {
+		id -> Int8,
+		club_id -> Int8,
+		index -> Int8,
+		png -> Blob,
+	}
 }
 
 #[derive(Queryable)]
 pub struct ClubImage {
-    pub id: i64,
-    pub club_id: i64,
-    pub index: i64,
-    pub png: Vec<u8>,
+	pub id: i64,
+	pub club_id: i64,
+	pub index: i64,
+	pub png: Vec<u8>,
 }
 
 // Variable number of club organizers, some may have more permissions (TODO)
 diesel::table! {
-    club_organizer (id) {
-        id -> Int8,
-        club_id -> Int8,
-        user_id -> Int8,
-    }
+	club_organizer (id) {
+		id -> Int8,
+		club_id -> Int8,
+		user_id -> Int8,
+	}
 }
 
 #[derive(Queryable)]
 pub struct ClubOrganizer {
-    pub id: i64,
-    pub club_id: i64,
-    pub user_id: i64,
+	pub id: i64,
+	pub club_id: i64,
+	pub user_id: i64,
 }
 
 // A specific event, which may be under a club
 diesel::table! {
-    event (id) {
-        id -> Int8,
-        club_id -> Nullable<Int8>,
-        name -> Varchar,
-        description_text -> Varchar,
-        description_html -> Varchar,
-        start -> Timestamp,
-    }
+	event (id) {
+		id -> Int8,
+		club_id -> Nullable<Int8>,
+		name -> Varchar,
+		description_text -> Varchar,
+		description_html -> Varchar,
+		start -> Timestamp,
+	}
 }
 
 #[derive(Queryable)]
 pub struct Event {
-    pub id: i64,
-    pub club_id: Option<i64>,
-    pub name: String,
-    pub description_text: String,
-    pub description_html: String,
-    pub start: NaiveDateTime,
+	pub id: i64,
+	pub club_id: Option<i64>,
+	pub name: String,
+	pub description_text: String,
+	pub description_html: String,
+	pub start: NaiveDateTime,
 }
 
 // Serializable structs for JSON transer
 #[derive(Queryable, Serialize, Deserialize)]
 pub struct EventJson {
-    pub id: i64,
-    pub name: String,
-    pub description: String,
-    pub start: i64,
+	pub id: i64,
+	pub name: String,
+	pub description: String,
+	pub start: i64,
 }
 
 // Variable number of event images
 diesel::table! {
-    event_image (id) {
-        id -> Int8,
-        event_id -> Int8,
-        index -> Int8,
-        png -> Blob,
-    }
+	event_image (id) {
+		id -> Int8,
+		event_id -> Int8,
+		index -> Int8,
+		png -> Blob,
+	}
 }
 
 #[derive(Queryable)]
 pub struct EventImage {
-    pub id: i64,
-    pub event_id: i64,
-    pub index: i64,
-    pub png: Vec<u8>,
+	pub id: i64,
+	pub event_id: i64,
+	pub index: i64,
+	pub png: Vec<u8>,
 }
 
 // An event category, every event can have as many categories as neccesary
 diesel::table! {
-    event_category (id) {
-        id -> Int8,
-        event_id -> Int8,
-        category -> Varchar,
-    }
+	event_category (id) {
+		id -> Int8,
+		event_id -> Int8,
+		category -> Varchar,
+	}
 }
 
 #[derive(Queryable)]
 pub struct EventCategory {
-    pub id: i64,
-    pub event_id: i64,
-    pub category: String,
+	pub id: i64,
+	pub event_id: i64,
+	pub category: String,
 }
 
 // An event rsvp, different tiers of rsvps
 diesel::table! {
-    event_rsvp (id) {
-        id -> Int8,
-        event_id -> Int8,
-        user_id -> Int8,
-    }
+	event_rsvp (id) {
+		id -> Int8,
+		event_id -> Int8,
+		user_id -> Int8,
+	}
 }
 
 #[derive(Queryable)]
 pub struct EventRsvp {
-    pub id: i64,
-    pub event_id: i64,
-    pub user_id: i64,
+	pub id: i64,
+	pub event_id: i64,
+	pub user_id: i64,
 }
 
 // Variable number of event organizers, some may have more permissions (TODO)
 diesel::table! {
-    event_organizer (id) {
-        id -> Int8,
-        event_id -> Int8,
-        user_id -> Int8,
-    }
+	event_organizer (id) {
+		id -> Int8,
+		event_id -> Int8,
+		user_id -> Int8,
+	}
 }
 
 #[derive(Queryable)]
 pub struct EventOrganizer {
-    pub id: i64,
-    pub event_id: i64,
-    pub user_id: i64,
+	pub id: i64,
+	pub event_id: i64,
+	pub user_id: i64,
 }
 
 // Users, some of which are organizers
 diesel::table! {
-    user (id) {
-        id -> Int8,
-        name -> Varchar,
-        email -> Varchar,
-        password -> Varchar,
-    }
+	user (id) {
+		id -> Int8,
+		name -> Varchar,
+		email -> Varchar,
+		password -> Varchar,
+	}
 }
 
 #[derive(Queryable)]
 pub struct User {
-    pub id: i64,
-    pub name: String,
-    pub email: String,
-    pub password: String,
+	pub id: i64,
+	pub name: String,
+	pub email: String,
+	pub password: String,
 }
 
 #[derive(Queryable, Serialize, Deserialize)]
 pub struct EventResultJson {
-    pub events: Vec<EventJson>,
+	pub events: Vec<EventJson>,
 }
 
 #[derive(Queryable, Serialize, Deserialize)]
 pub struct ClubResultJson {
-    pub clubs: Vec<ClubJson>,
+	pub clubs: Vec<ClubJson>,
 }
 
 pub fn create_database() -> PgConnection {
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let mut conn = PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+	let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+	let mut conn = PgConnection::establish(&database_url)
+		.unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
 
-    // Create tables one by one
-    diesel::sql_query(
-        r#"
-		CREATE TABLE club IF NOT EXISTS {
+	// Create tables one by one
+	diesel::sql_query(
+		r#"
+		CREATE TABLE IF NOT EXISTS club (
 			id BIGINT primary key,
 			name VARCHAR(256),
 			description_text VARCHAR(256),
 			description_html VARCHAR(256)
-		}
+		)
 	"#,
-    )
-    .execute(&mut conn)
-    .unwrap();
+	)
+	.execute(&mut conn)
+	.unwrap();
+	diesel::sql_query(
+		r#"
+		CREATE TABLE IF NOT EXISTS club_image (
+			id BIGINT primary key,
+			club_id BIGINT,
+			index BIGINT,
+			png BYTEA
+		)
+	"#,
+	)
+	.execute(&mut conn)
+	.unwrap();
 
-    conn
+	conn
 }
 
 fn get_all_events(conn: &mut PgConnection) -> Option<Vec<Event>> {
-    let result = event::table.limit(10).get_results::<Event>(conn);
-    if let Ok(events) = result {
-        Some(events)
-    } else {
-        None
-    }
+	let result = event::table.limit(10).get_results::<Event>(conn);
+	if let Ok(events) = result {
+		Some(events)
+	} else {
+		None
+	}
 }
 
 pub fn get_all_events_json(conn: &mut PgConnection) -> Option<String> {
-    // Query 10 events for now
-    let result: Result<Vec<Event>, diesel::result::Error> =
-        event::table.limit(10).get_results(conn);
-    if result.is_err() {
-        return None;
-    }
-    let events = result.unwrap();
+	// Query 10 events for now
+	let result: Result<Vec<Event>, diesel::result::Error> =
+		event::table.limit(10).get_results(conn);
+	if result.is_err() {
+		return None;
+	}
+	let events = result.unwrap();
 
-    let events_result = EventResultJson {
-        events: events
-            .iter()
-            .map(|event| EventJson {
-                id: event.id,
-                name: event.name.clone(),
-                description: event.description_text.clone(),
-                start: event.start.timestamp(),
-            })
-            .collect(),
-    };
+	let events_result = EventResultJson {
+		events: events
+			.iter()
+			.map(|event| EventJson {
+				id: event.id,
+				name: event.name.clone(),
+				description: event.description_text.clone(),
+				start: event.start.timestamp(),
+			})
+			.collect(),
+	};
 
-    let json = serde_json::to_string(&events_result);
-    if json.is_err() {
-        return None;
-    }
+	let json = serde_json::to_string(&events_result);
+	if json.is_err() {
+		return None;
+	}
 
-    Some(json.unwrap())
+	Some(json.unwrap())
 }
 
 fn get_num_rsvps_json(event_id: i64, conn: &mut PgConnection) -> Option<String> {
-    // Query 10 events for now
-    let result: Result<i64, diesel::result::Error> = event_rsvp::table
-        .filter(event_rsvp::event_id.eq(event_id))
-        .count()
-        .get_result(conn);
-    if result.is_err() {
-        return None;
-    }
-    let num_rsvps = result.unwrap();
+	// Query 10 events for now
+	let result: Result<i64, diesel::result::Error> = event_rsvp::table
+		.filter(event_rsvp::event_id.eq(event_id))
+		.count()
+		.get_result(conn);
+	if result.is_err() {
+		return None;
+	}
+	let num_rsvps = result.unwrap();
 
-    let json = serde_json::to_string(&json!({ "num_rsvps": num_rsvps }));
-    if json.is_err() {
-        return None;
-    }
+	let json = serde_json::to_string(&json!({ "num_rsvps": num_rsvps }));
+	if json.is_err() {
+		return None;
+	}
 
-    Some(json.unwrap())
+	Some(json.unwrap())
 }
 
 pub fn get_club_image(id: i64, conn: &mut PgConnection) -> Option<Vec<u8>> {
-    // Query for image
-    let result: Result<EventImage, diesel::result::Error> = club_image::table
-        .filter(club_image::id.eq(id))
-        .get_result(conn);
-    if result.is_err() {
-        return None;
-    }
+	// Query for image
+	let result: Result<EventImage, diesel::result::Error> = club_image::table
+		.filter(club_image::id.eq(id))
+		.get_result(conn);
+	if result.is_err() {
+		return None;
+	}
 
-    Some(result.unwrap().png)
+	Some(result.unwrap().png)
 }
 
 pub fn get_all_clubs_json(conn: &mut PgConnection) -> Option<String> {
-    // Query 10 clubs for now
-    let result: Result<Vec<Club>, diesel::result::Error> = club::table.limit(10).get_results(conn);
-    if result.is_err() {
-        return None;
-    }
-    let events = result.unwrap();
+	// Query 10 clubs for now
+	let result: Result<Vec<Club>, diesel::result::Error> = club::table.limit(10).get_results(conn);
+	if result.is_err() {
+		return None;
+	}
+	let events = result.unwrap();
 
-    let events_result = ClubResultJson {
-        clubs: events
-            .iter()
-            .map(|club| ClubJson {
-                id: club.id,
-                name: club.name.clone(),
-                description_text: club.description_text.clone(),
-                description_html: club.description_html.clone(),
-            })
-            .collect(),
-    };
+	let events_result = ClubResultJson {
+		clubs: events
+			.iter()
+			.map(|club| ClubJson {
+				id: club.id,
+				name: club.name.clone(),
+				description_text: club.description_text.clone(),
+				description_html: club.description_html.clone(),
+			})
+			.collect(),
+	};
 
-    let json = serde_json::to_string(&events_result);
-    if json.is_err() {
-        return None;
-    }
+	let json = serde_json::to_string(&events_result);
+	if json.is_err() {
+		return None;
+	}
 
-    Some(json.unwrap())
+	Some(json.unwrap())
 }
 
 pub fn get_event_json(id: i64, conn: &mut PgConnection) -> Option<String> {
-    // Query 10 events for now
-    let result: Result<Event, diesel::result::Error> =
-        event::table.filter(event::id.eq(id)).get_result(conn);
-    if result.is_err() {
-        return None;
-    }
-    let event = result.unwrap();
+	// Query 10 events for now
+	let result: Result<Event, diesel::result::Error> =
+		event::table.filter(event::id.eq(id)).get_result(conn);
+	if result.is_err() {
+		return None;
+	}
+	let event = result.unwrap();
 
-    let json = serde_json::to_string(&EventJson {
-        id: event.id,
-        name: event.name.clone(),
-        description: event.description_text.clone(),
-        start: event.start.timestamp(),
-    });
-    if json.is_err() {
-        return None;
-    }
+	let json = serde_json::to_string(&EventJson {
+		id: event.id,
+		name: event.name.clone(),
+		description: event.description_text.clone(),
+		start: event.start.timestamp(),
+	});
+	if json.is_err() {
+		return None;
+	}
 
-    Some(json.unwrap())
+	Some(json.unwrap())
+}
+
+// Query newest events in order
+pub fn get_newest_events_json(num: i64, conn: &mut PgConnection) -> Option<String> {
+	let result: Result<Event, diesel::result::Error> = event::table
+		.order(event::start.desc())
+		.limit(num)
+		.get_result(conn);
+	if result.is_err() {
+		return None;
+	}
+	let event = result.unwrap();
+
+	let json = serde_json::to_string(&EventJson {
+		id: event.id,
+		name: event.name.clone(),
+		description: event.description_text.clone(),
+		start: event.start.timestamp(),
+	});
+	if json.is_err() {
+		return None;
+	}
+
+	Some(json.unwrap())
+}
+
+// Add event with data, return id
+pub fn add_event_json(data: String, conn: &mut PgConnection) -> Option<i64> {
+	/*
+	let event_struct: Result<EventJson, serde_json::Error> = serde_json::from_str(&data.as_str());
+	if event_struct.is_err() {
+		None
+	}
+
+	diesel::insert_into(event).values((
+		event::name.eq(event_struct.unwrap().name),
+		event::description_text.eq(event_struct.unwrap().description),
+		event::start.eq(event_struct.unwrap().start),
+	));
+
+	let result: Result<Event, diesel::result::Error> = event::table
+		.order(event::start.desc())
+		.limit(num)
+		.get_result(conn);
+	if result.is_err() {
+		return None;
+	}
+	let event = result.unwrap();
+
+	let json = serde_json::to_string(&EventJson {
+		id: event.id,
+		name: event.name.clone(),
+		description: event.description_text.clone(),
+		start: event.start.timestamp(),
+	});
+	if json.is_err() {
+		return None;
+	}
+
+	Some(json.unwrap())
+	*/
+	None
 }
