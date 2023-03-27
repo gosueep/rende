3645c0fccf7ac2e0fbf0bd5e6d1f969fd2d259c4
gosueep <eugin03@gmail.com>
2023-03-27 02:12:58 -0600

    WIP - event population on home page, async fetching not updating

--- src/SearchPage.tsx
+++ src/SearchPage.tsx
@@ -1,27 +1,73 @@
 import { Link } from "@solidjs/router"
-import { Component } from "solid-js"
-import { createSignal, createEffect } from 'solid-js';
+import { Component, For } from "solid-js"
+import { createSignal, createEffect, onMount } from 'solid-js';
 
 import NavBar from "./NavBar"
 import SearchBar from "./SearchBar"
 import EventCard from "./EventCard"
 
-interface Event {
-	name: string
+import type {EventInfoType, EventType} from "./EventTypes"
+import {fetchEvents} from "./EventTypes"
+
+
+const EventCardGrid: Component<{}> = () => {
+	const [events, setEvents] = createSignal<EventType[]>([]);
+
+	console.log(events())
+	console.log("SDFSDFSDF")
+	console.log(events() === undefined || events().length == 0)
+	createEffect(async () => {
+		const res = await fetchEvents(20) as EventType[]
+        setEvents(res)
+		// console.log(events())
+		// console.log("DONE UP HERE")
+	});
+
+	// if (events() === undefined || events().length == 0) {
+	// 	return <div>Loading...</div>
+	// }
+
+	return (
+	<For each={events()}>{(event) =>
+		<div>
+			<EventCard event_id={event.info.id}></EventCard>
+			<p>bruh</p>
+		</div>
+	}</For>
+	)
 }
 
+
 const SearchPage: Component<{}> = () => {
-	const [events, setEvents] = createSignal<Event[]>([]);
+	const [events, setEvents] = createSignal<EventType[]>([]);
 
-	createEffect(() => {
-		(async function() {
-			let events_req = await fetch("http://127.0.0.1:3030/get_events");
-			let events_json = await events_req.json();
-			setEvents(events_json);
-		})()
-	  }, [setEvents]);
+	// onMount(() => {
+	// 	fetch('http://localhost:3030/get_newest_events/20')
+	// 		.then(response => response.json())
+	// 		.then(events => {
+	// 			console.log(events)
+	// 			setEvents(events)
+	// 		})
+	// })
 
-	  return <>
+	// onMount(async () => {
+    //     const res = await fetchEvents(20) as EventType[]
+    //     setEvents(res)
+	// 	console.log(events())
+    // });
+
+	createEffect(async () => {
+		const res = await fetchEvents(20) as EventType[]
+        setEvents(res)
+		// console.log(events())
+	});
+
+
+	// if (events() === undefined || events().length == 0) {
+	// 	return <div>Loading...</div>
+	// }
+
+	return <>
 		<NavBar></NavBar>
 		<div class="flex flex-row">
 			{/* <div class="flex-none" id="dashboard-left">
@@ -30,21 +76,17 @@ const SearchPage: Component<{}> = () => {
 			<div class="flex-1" id="content">
 				<div class="flex flex-col">
 					<div class="grid gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-5">
-						<EventCard></EventCard>
-						<EventCard></EventCard>
-						<EventCard></EventCard>
-						<EventCard></EventCard>
-						<EventCard></EventCard>
-						<EventCard></EventCard>
-						<EventCard></EventCard>
-						<EventCard></EventCard>
-						<div class="flex-1" id="grid-events">
-							<div class="grid grid-flow-row-dense grid-cols-5">
-								{events().map((event) => {
-									return <div>{event.name}</div>
-								})}
-							</div>
+						<EventCard event_id={1}></EventCard>
+						<EventCard event_id={2}></EventCard>
+						<EventCard event_id={3}></EventCard>
+						<EventCard event_id={4}></EventCard>
+						<EventCardGrid></EventCardGrid>
+						{/* <For each={events()}>{(event) =>
+						<div>
+							<EventCard event_id={event.info.id}></EventCard>
+							<p>bruh</p>
 						</div>
+						}</For> */}
 					</div>
 				</div>
 			</div>
