ecf70823dcd0b6dc4274d1f3cdc33e5dace9c325
Scott Enriquez <enriquezscott@mines.edu>
2023-04-06 14:31:12 -0600

    Uniform tiles

--- src/EventCard.tsx
+++ src/EventCard.tsx
@@ -15,20 +15,26 @@ const EventCard = (props: any) => {
     });
 
     return (
-        <div class="max-h-sm min-h-sm rounded overflow-hidden shadow-lg">
-            {event.images.length == 0 ? <p>no image</p> : <a href="">
-                <img class="object-fill" src={"/get_event_image/" + event.images[0]} />
-            </a>}
-            <div class="px-6 py-4">
-                <h2>Name: {event.info.name}</h2>
-                <h3>Location: {location()?.description}</h3>
-                <h3>Start: {new Date((event.info.start ?? 0) * 1000).toLocaleTimeString([], { weekday: "long", hour: "numeric", minute: "2-digit" })}</h3>
-                <h3>Description: {event.info.description_text}</h3>
-            </div>
-            <div class="px-6 pt-4 pb-2">
-                <For each={event.categories}>{(tag, i) =>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">{tag}</span>
-                }</For>
+        <div class="w-80 h-80 bg-white rounded overflow-hidden shadow-lg">
+            <div class="h-full flex flex-col overflow-y-auto">
+                <div class="h-40 relative">
+                    {event.images.length == 0 ? <p class="absolute inset-0 flex items-center justify-center">no image</p> : <a href="">
+                        <img class="w-full h-full object-cover" src={"/get_event_image/" + event.images[0]} />
+                    </a>}
+                </div>
+                <div class="px-6 py-4 flex flex-col justify-between flex-grow">
+                    <div>
+                        <h2 class="font-bold text-xl mb-2">Name: {event.info.name}</h2>
+                        <h3 class="text-sm mb-2">Location: {location()?.description}</h3>
+                        <h3 class="text-sm mb-2">Start: {new Date((event.info.start ?? 0) * 1000).toLocaleTimeString([], { weekday: "long", hour: "numeric", minute: "2-digit" })}</h3>
+                    </div>
+                    <div class="text-sm mb-2">Description: {event.info.description_text}</div>
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                    <For each={event.categories}>{(tag, i) =>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 mb-2">{tag}</span>
+                    }</For>
+                </div>
             </div>
         </div >
     )
